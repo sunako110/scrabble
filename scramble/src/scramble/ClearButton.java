aef0e992dd71cab2d0b4ed2dbf32dac89264a280
qianfanc <36269555+qianfanc@users.noreply.github.com>
2018-09-23 00:44:08 +1200

    Create ClearButton.java

---
+++ scramble/src/scramble/ClearButton.java
@@ -0,0 +1,24 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class ClearButton extends JButton implements ActionListener {
+	private static final long serialVersionUID = 1L;
+	
+	public ClearButton(String text) {
+		this.setText(text);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
