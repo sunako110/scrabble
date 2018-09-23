5e71496dfc1d59b01ec7539d07f2395f4cadc9ac
sunako110 <43470631+sunako110@users.noreply.github.com>
2018-09-24 00:39:13 +1000

    Update mainwindow.java
    刷新LetterBar的功能放到了LetterButton的class里面

--- scramble/src/scramble/mainwindow.java
+++ scramble/src/scramble/mainwindow.java
@@ -3,41 +3,13 @@ package scramble;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 public class mainwindow{
-	ImageIcon A;
-	ImageIcon B;
-	ImageIcon C;
-	ImageIcon D;
-	ImageIcon E;
-	ImageIcon F;
-	ImageIcon G;
-	ImageIcon H;
-	ImageIcon I;
-	ImageIcon J;
-	ImageIcon K;
-	ImageIcon L;
-	ImageIcon M;
-	ImageIcon N;
-	ImageIcon O;
-	ImageIcon P;
-	ImageIcon Q;
-	ImageIcon R;
-	ImageIcon S;
-	ImageIcon T;
-	ImageIcon U;
-	ImageIcon V;
-	ImageIcon W;
-	ImageIcon X;
-	ImageIcon Y;
-	ImageIcon Z;
 	
 	JFrame gameFrame = new JFrame("Scramble");
 	JButton clearButton = new JButton("clear");
@@ -182,150 +154,8 @@ public class mainwindow{
 					}
 				}	
 				for(int i = 0; i < 7; i++) {
-					
 					letters[i].setEnabled(true);
-					
-					A = new ImageIcon("src/image/letter-a.png");
-					B = new ImageIcon("src/image/letter-b.png");
-					C = new ImageIcon("src/image/letter-c.png");
-					D = new ImageIcon("src/image/letter-d.png");
-					E = new ImageIcon("src/image/letter-e.png");
-					F = new ImageIcon("src/image/letter-f.png");
-					G = new ImageIcon("src/image/letter-g.png");
-					H = new ImageIcon("src/image/letter-h.png");
-					I = new ImageIcon("src/image/letter-i.png");
-					J = new ImageIcon("src/image/letter-j.png");
-					K = new ImageIcon("src/image/letter-k.png");
-					L = new ImageIcon("src/image/letter-l.png");
-					M = new ImageIcon("src/image/letter-m.png");
-					N = new ImageIcon("src/image/letter-n.png");
-					O = new ImageIcon("src/image/letter-o.png");
-					P = new ImageIcon("src/image/letter-p.png");
-					Q = new ImageIcon("src/image/letter-q.png");
-					R = new ImageIcon("src/image/letter-r.png");
-					S = new ImageIcon("src/image/letter-s.png");
-					T = new ImageIcon("src/image/letter-t.png");
-					U = new ImageIcon("src/image/letter-u.png");
-					V = new ImageIcon("src/image/letter-v.png");
-					W = new ImageIcon("src/image/letter-w.png");
-					X = new ImageIcon("src/image/letter-x.png");
-					Y = new ImageIcon("src/image/letter-y.png");
-					Z = new ImageIcon("src/image/letter-z.png");
-					
-					Random r = new Random();
-					char c = (char) (r.nextInt(26) + 'a');
-					char letter = c;
-					
-					switch(letter) {
-					case (char)'a':
-						letters[i].setIcon(A);
-						letters[i].letter = letter;
-						break;
-					case (char)'b':
-						letters[i].setIcon(B);
-						letters[i].letter = letter;
-						break;
-					case (char)'c':
-						letters[i].setIcon(C);
-						letters[i].letter = letter;
-						break;
-					case (char)'d':
-						letters[i].setIcon(D);
-						letters[i].letter = letter;
-						break;
-					case (char)'e':
-						letters[i].setIcon(E);
-						letters[i].letter = letter;
-						break;
-					case (char)'f':
-						letters[i].setIcon(F);
-						letters[i].letter = letter;
-						break;
-					case (char)'g':
-						letters[i].setIcon(G);
-						letters[i].letter = letter;
-						break;
-					case (char)'h':
-						letters[i].setIcon(H);
-						letters[i].letter = letter;
-						break;
-					case (char)'i':
-						letters[i].setIcon(I);
-						letters[i].letter = letter;
-						break;
-					case (char)'j':
-						letters[i].setIcon(J);
-						letters[i].letter = letter;
-						break;
-					case (char)'k':
-						letters[i].setIcon(K);
-						letters[i].letter = letter;
-						break;
-					case (char)'l':
-						letters[i].setIcon(L);
-						letters[i].letter = letter;
-						break;
-					case (char)'m':
-						letters[i].setIcon(M);
-						letters[i].letter = letter;
-						break;
-					case (char)'n':
-						letters[i].setIcon(N);
-						letters[i].letter = letter;
-						break;
-					case (char)'o':
-						letters[i].setIcon(O);
-						letters[i].letter = letter;
-						break;
-					case (char)'p':
-						letters[i].setIcon(P);
-						letters[i].letter = letter;
-						break;
-					case (char)'q':
-						letters[i].setIcon(Q);
-						letters[i].letter = letter;
-						break;
-					case (char)'r':
-						letters[i].setIcon(R);
-						letters[i].letter = letter;
-						break;
-					case (char)'s':
-						letters[i].setIcon(S);
-						letters[i].letter = letter;
-						break;
-					case (char)'t':
-						letters[i].setIcon(T);
-						letters[i].letter = letter;
-						break;
-					case (char)'u':
-						letters[i].setIcon(U);
-						letters[i].letter = letter;
-						break;
-					case (char)'v':
-						letters[i].setIcon(V);
-						letters[i].letter = letter;
-						break;
-					case (char)'w':
-						letters[i].setIcon(W);
-						letters[i].letter = letter;
-						break;
-					case (char)'x':
-						letters[i].setIcon(X);
-						letters[i].letter = letter;
-						break;
-					case (char)'y':
-						letters[i].setIcon(Y);
-						letters[i].letter = letter;
-						break;
-					case (char)'z':
-						letters[i].setIcon(Z);
-					    letters[i].letter = letter;
-						break;
-					default:
-						letters[i].setIcon(null);
-						letters[i].letter = letter;
-						break;
-					}
+					letters[i].refreshLetter();
 				}
 			}
 		};
