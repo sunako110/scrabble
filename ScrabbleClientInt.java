3a78940db6753bf261d46add1b3446e0b5488f1d
qianfanc <36269555+qianfanc@users.noreply.github.com>
2018-10-03 13:41:31 +1300

    Delete ScrabbleClientInt.java

--- ScrabbleClientInt.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.*;
-import java.util.ArrayList;
- 
-
-public interface ScrabbleClientInt extends Remote{
-	public void update (String name)throws RemoteException ;
-	public String getName()throws RemoteException ;
-	public void addPlayer(ArrayList<String> s) throws RemoteException;
-}
