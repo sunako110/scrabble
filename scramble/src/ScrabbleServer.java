7e87bce5bd1b76eb87fbe66cf5df9f2ce191ab34
sunako110 <43470631+sunako110@users.noreply.github.com>
2018-10-03 18:06:59 +1000

    Add files via upload

--- scramble/src/ScrabbleServer.java
+++ scramble/src/ScrabbleServer.java
@@ -6,9 +6,9 @@ import java.util.*;
 
 public class ScrabbleServer extends UnicastRemoteObject implements ScrabbleServerInt {
 
-	private Vector<ScrabbleClientInt> v=new Vector<ScrabbleClientInt>();
-	ArrayList<ScrabblePlayer> playerList = new ArrayList<ScrabblePlayer>();
+	private Vector<ScrabbleClientInt> playerList=new Vector<ScrabbleClientInt>();
 	ArrayList<String> nameList = new ArrayList<String>();
+	ScrabbleGame game;
 	
 	
 	protected ScrabbleServer() throws RemoteException {
@@ -21,11 +21,11 @@ public class ScrabbleServer extends UnicastRemoteObject implements ScrabbleServe
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	public boolean login(ScrabbleClientInt a) throws RemoteException {
+	synchronized public boolean login(ScrabbleClientInt a) throws RemoteException {
 		System.out.println(a.getName() + "  got connected....");	
 		//a.tell("You have Connected successfully.");
 		//publish(a.getName()+ " has just connected.");
-		v.add(a);
+		playerList.add(a);
 		return true;	
 	}
 
@@ -53,30 +53,43 @@ public class ScrabbleServer extends UnicastRemoteObject implements ScrabbleServe
 
 	@Override
 	public Vector<ScrabbleClientInt> getConnected() throws RemoteException {
-		return v;
+		return playerList;
 	}
 
 	@Override
-	public void publish(String s) throws RemoteException {
-		playerList.clear();
+	synchronized public void publish(String s) throws RemoteException {
 		nameList.clear();
-		for(int i=0;i<v.size();i++){
-			ScrabbleClientInt n=(ScrabbleClientInt)v.get(i);
-			playerList.add(new ScrabblePlayer(n.getName()));
+		for(int i=0;i<playerList.size();i++){
+			ScrabbleClientInt n=(ScrabbleClientInt)playerList.get(i);
 			nameList.add(n.getName());
 		}
 	
 		
-			for(int i=0;i<v.size();i++){
+			for(int i=0;i<playerList.size();i++){
 			    try{
-			     	ScrabbleClientInt tmp=(ScrabbleClientInt)v.get(i);
+			     	ScrabbleClientInt tmp=(ScrabbleClientInt)playerList.get(i);
 			     	tmp.addPlayer(nameList);
 			    }catch(Exception e){
 			    	//problem with the client not connected.
 			    	//Better to remove it
 			    }
 			}
+			
+			for(int i = 0; i <playerList.size()-1;i++) {
+				ScrabbleClientInt tmp=(ScrabbleClientInt)playerList.get(i);
+				tmp.cannotStartGame();
+			}
 		
+	}
+
+	@Override
+	synchronized public void startGame() throws RemoteException {
+		for(int i = 0; i < playerList.size()-1; i++) {
+			System.out.println(playerList.get(i).getName());
+			ScrabbleClientInt tmp = (ScrabbleClientInt)playerList.get(i);
+			tmp.startGame();
+			
+		}
 	}
 
 }
