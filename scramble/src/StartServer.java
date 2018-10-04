ee14f97b1739e7ef5fb20a368a95e2c8b232ffd0
qianfanc <36269555+qianfanc@users.noreply.github.com>
2018-10-04 19:22:56 +1300

    Update StartServer.java

--- scramble/src/StartServer.java
+++ scramble/src/StartServer.java
@@ -1,13 +1,27 @@
 import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+//import java.rmi.RMISecurityManager;
 
 public class StartServer {
+	
 	public static void main(String[] args) {
+		Registry registry;
+		
 		try {
+			
 				//System.setSecurityManager(new RMISecurityManager());
-			 	java.rmi.registry.LocateRegistry.createRegistry(1099);
-			 	
+				//System.setProperty("java.security.policy","file:///home/.../<filename>.policy");
+			 //	java.rmi.registry.LocateRegistry.createRegistry(1099);
+				
 				ScrabbleServerInt b=new ScrabbleServer();	
-				Naming.rebind("rmi://10.13.90.195/myabc", b);
+			 	
+			 	registry = LocateRegistry.createRegistry(1099);
+			 	registry.rebind("rmi://10.13.90.195/myabc", b);
+			 	
+			 	
+				
+				//Naming.rebind("rmi://10.13.90.195/myabc", b);
 				System.out.println("[System] Server is ready.");
 			}catch (Exception e) {
 					System.out.println("Server failed: " + e);
